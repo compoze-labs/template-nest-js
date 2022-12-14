52eba6055fb09299d3eddf15a1f285e2d24710ae
Andrew Larsen <andrew@compozelabs.com>
2022-12-14 01:31:57 -0600

    add wait for stable deployment

--- scripts/restart_service.sh
+++ scripts/restart_service.sh
@@ -3,11 +3,15 @@
 set -euo pipefail
 
 restart() {
-
+    
     SERVICE=$1
     STAGE=$2
     echo "creating deployment for ${SERVICE}-${STAGE}"
-
-    aws ecs update-service --cluster ${SERVICE}-${STAGE} --service ${SERVICE}-${STAGE} --force-new-deployment
     
+    
+    output=$(aws ecs update-service --cluster ${SERVICE}-${STAGE} --service ${SERVICE}-${STAGE} --force-new-deployment 2>&1)
+    echo "${output}" | jq ".service.taskDefinition"
+    
+    echo "waiting for deployment to become stable"
+    aws ecs wait services-stable --cluster "${SERVICE}-${STAGE}" --service "${SERVICE}-${STAGE}"
 }
